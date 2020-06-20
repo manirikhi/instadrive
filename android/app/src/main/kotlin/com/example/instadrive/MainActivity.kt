afd3e2edb31481fea90cc0847bdec78a1d96782e
Manjit Singh <manjit.singh@mobilelive.ca>
2020-06-20 17:40:37 -0400

    config changes

--- android/app/src/main/kotlin/com/example/instadrive/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/instadrive/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.instadrive
+package com.techbyte.instadrive
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
- import 'package:google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:instadrive/src/login.dart';
 
 void main() => runApp(MyApp());
